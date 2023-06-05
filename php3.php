6e809dd389b7b8ea98c90dead4654e804ae7aa67
alfarabygit <alfarabyindra@gmail.com>
2023-06-05 21:20:08 +0700

    upload file php

---
+++ php3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+    <?php
+    // tipe data di PHP
+    $int = 100;
+    $float = 75.5;
+    $string = "Belajar PHP";
+    $isActive = true;
+    $x = null;
+    $color = array("Red", "Green", "Blue");
+
+
+    echo "Type data integer is .$int <br/>";
+    echo "Type data float is .$float <br/>";
+    echo "Type data string is .$string <br/>";
+    echo "Type data boolean is .$boolean <br/>";
+    echo "Type data array is .$array[0] <br/>";
+    echo "Type data null is .$x <br/>";
+
+    ?>
+</body>
+
+</html>
